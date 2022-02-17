4327ef21a2404ac404c397ca47e3b583c25cade0
masor1 <masor.dev@gmail.com>
2022-02-17 15:29:44 +0300

    lesson 1 - domain

--- app/src/main/java/com/masorone/addingnumbers/presentation/MainActivity.kt
+++ app/src/main/java/com/masorone/addingnumbers/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.masorone.addingnumbers
+package com.masorone.addingnumbers.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.masorone.addingnumbers.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
